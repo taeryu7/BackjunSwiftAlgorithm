a288d7604a8a728b9f1fc5c8db4017aa635c4a4a
taeryu7 <taeryu97@gmail.com>
2024-11-09 20:53:19 +0900

    1-8

---
+++ MyPlayground.playground/Pages/1-8.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ angle:Int) -> Int {
+    if angle >= 0 && angle < 90 {
+        return 1
+    } else if angle == 90 {
+        return 2
+    } else if angle > 90 && angle < 180 {
+        return 3
+    } else if angle == 180 {
+        return 4
+    } else {
+        return -1
+    }
+}
+
+//: [Next](@next)
