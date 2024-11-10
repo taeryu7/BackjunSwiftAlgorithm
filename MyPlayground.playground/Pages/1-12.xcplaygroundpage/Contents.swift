ebb6ec7018e4322109ec03130cfd3a10694da682
taeryu7 <taeryu97@gmail.com>
2024-11-10 16:17:50 +0900

    1-12 평균구하기

---
+++ MyPlayground.playground/Pages/1-12.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ arr: [Int]) -> Double {
+    let sum = arr.reduce(0, +)
+    return Double(sum) / Double(arr.count)
+}
+
+//: [Next](@next)
