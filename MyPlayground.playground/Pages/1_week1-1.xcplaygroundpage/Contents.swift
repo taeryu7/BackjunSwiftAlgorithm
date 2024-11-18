92ebe059ac39c600647eb81c08e94598dc754534
taeryu7 <taeryu97@gmail.com>
2024-11-18 09:52:47 +0900

    1주차 합동 알고리즘 1-1

---
+++ MyPlayground.playground/Pages/1_week1-1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ numbers:[Int]) -> [Int] {
+    var answer:[Int] = []
+    for i in numbers {
+        answer.append(i*2)
+    }
+    return answer
+}
+
+//: [Next](@next)
