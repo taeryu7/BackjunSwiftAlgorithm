a84e3ced9a31ac8b12d9bfeaff335d07ed9a3837
taeryu7 <taeryu97@gmail.com>
2024-11-19 09:26:29 +0900

    첫수차 알고리즘 1-2 문자열 섞기

---
+++ MyPlayground.playground/Pages/1_week1-2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,8 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ str1:String, _ str2:String) -> String {
+    return zip(str1, str2).map { String($0) + String($1) }.joined()
+}
+//: [Next](@next)
