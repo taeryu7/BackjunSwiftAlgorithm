1d82d380415867d538458fbb9fd63eb7d73e91c9
taeryu7 <taeryu97@gmail.com>
2024-12-04 09:18:09 +0900

    2주차 알고리즘 1-1 카운트다운

---
+++ MyPlayground.playground/Pages/2_week1-1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func solution(_ start:Int, _ end_num:Int) -> [Int] {
+    return stride(from: start, through: end_num, by: -1).map{ $0}
+}
+
+
+//: [Next](@next)
